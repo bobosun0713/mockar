10ded9f0784f779f94e455fdfd4f01d461727ddb
bobosun0713 <chiabo.sun@gmail.com>
2025-11-04 23:53:54 +0800

    chore: build tools

---
+++ eslint.config.mjs
@@ -0,0 +1,114 @@
+import eslint from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { createTypeScriptImportResolver } from "eslint-import-resolver-typescript";
+import * as pluginImportX from "eslint-plugin-import-x";
+import eslintPluginReact from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  globalIgnores(["node_modules", ".output/", ".wxt", "dist", ".cache", "public"]),
+
+  eslint.configs.recommended,
+  pluginImportX.flatConfigs.recommended,
+  pluginImportX.flatConfigs.typescript,
+  {
+    name: "import-x",
+    rules: {
+      "import-x/first": "error",
+      "import-x/newline-after-import": "error",
+      "import-x/no-duplicates": "error",
+
+      // https://typescript-eslint.io/troubleshooting/typed-linting/performance/
+      "import-x/named": "off",
+      "import-x/namespace": "off",
+      "import-x/default": "off",
+      "import-x/no-named-as-default-member": "off",
+      "import-x/no-unresolved": "off"
+    },
+    settings: {
+      "import-x/resolver-next": [
+        createTypeScriptImportResolver({
+          alwaysTryTypes: true,
+          project: ["./tsconfig.json"]
+        })
+      ]
+    }
+  },
+
+  {
+    name: "simple-import-sort",
+    plugins: {
+      "simple-import-sort": simpleImportSort
+    },
+    rules: {
+      "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^react", "^@?\\w"],
+            ["^(@|components)(/.*|$)"],
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+            ["^.+\\.?(css)$"],
+            ["^\\u0000"]
+          ]
+        }
+      ]
+    }
+  },
+
+  {
+    name: "react",
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      eslintPluginReact.configs.flat.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite
+    ],
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.json"],
+        tsconfigRootDir: import.meta.dirname
+      },
+      globals: globals.browser
+    },
+    rules: {
+      "no-console": "off",
+      "no-debugger": "off",
+      "no-void": ["error", { allowAsStatement: true }],
+      "no-param-reassign": "off",
+      "func-names": "off",
+
+      // https://legacy.reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#eslint
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-import-type-side-effects": "error",
+      "@typescript-eslint/no-unused-vars": ["error", { varsIgnorePattern: "^_" }]
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  },
+
+  {
+    name: "specific-rules",
+    files: ["**/?(*.)*?([cm])js"],
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "off"
+    }
+  },
+
+  eslintConfigPrettier
+]);
