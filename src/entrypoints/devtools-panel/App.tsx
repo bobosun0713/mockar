f7ee6bf29fefb8ec48801ef6984349934d2f0d5e
bobosun0713 <chiabo.sun@gmail.com>
2025-12-30 23:41:05 +0800

    feat(devtools-panel): adjust layout

--- src/entrypoints/devtools-panel/App.tsx
+++ src/entrypoints/devtools-panel/App.tsx
@@ -1,20 +1,42 @@
+import { useState } from "react";
+import { Box } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 
+import EditorArea from "@/components/EditorArea";
 import Header from "@/components/Header";
-
-const theme = createTheme({
-  colorSchemes: {
-    light: true,
-    dark: true
-  }
-});
+import Sidebar from "@/components/Sidebar";
+import theme from "@/theme";
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return (
     <ThemeProvider defaultMode="light" theme={theme}>
       <CssBaseline></CssBaseline>
-      <Header></Header>
+
+      <Box
+        sx={{
+          minWidth: 1024,
+          height: "100%",
+          overflow: "hidden"
+        }}
+      >
+        <Header
+          onToggleSidebar={() => {
+            setIsSidebarOpen(!isSidebarOpen);
+          }}
+        ></Header>
+
+        <Sidebar
+          isOpen={isSidebarOpen}
+          onToggleSidebar={() => {
+            setIsSidebarOpen(!isSidebarOpen);
+          }}
+        ></Sidebar>
+
+        <EditorArea></EditorArea>
+      </Box>
     </ThemeProvider>
   );
 }
